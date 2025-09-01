0b3bbd4ac88bca7cfeea67c6df7236e04b79b90b
Carlos Valderrama <_CarlosValderrama@outlook.com>
2025-09-01 17:30:26 -0500

    feat: Basic structure from BigArticled finished, Articles adapted to render BigArticle and Article components.

--- src/components/articles.tsx
+++ src/components/articles.tsx
@@ -6,8 +6,6 @@ const positions: Job[] = homePageData.openPositions;
 const articles: BlogPreview[] = homePageData.blogPreview;
 const authors = homePageData.authors;
 
-
-
 function Author(author: Author) {
   return (
     <div className="text-sm text-gray-600 flex items-center gap-2 mt-4">
@@ -17,7 +15,9 @@ function Author(author: Author) {
         className="w-8 h-8 rounded-full object-cover"
       />
       <span>By</span>
-      <a href={`authors/${author.slug}`}>{author.name}</a>
+      <a href={`authors/${author.slug}`} className="underline font-bold">
+        {author.name}
+      </a>
     </div>
   );
 }
@@ -52,16 +52,45 @@ function Article(article: BlogPreview) {
   );
 }
 
-
+function BigArticle(article: BlogPreview) {
+  return (
+    <div className="flex h-[460px] mx-4 mb-16 box-border h-">
+      <a href={article.slug} className="flex-shrink-0 ">
+        <img
+          src={article.coverImage}
+          alt="Image"
+          className="min-w-sm object-cover rounded-4xl h-[460px] w-[502px] flex-shrink-0"
+        />
+      </a>
+      <div className="flex flex-col max-w-xlg justify-center my-10 p-10">
+        <span className="text-back font-bold text-sm">LATEST</span>
+        <div>
+          <a href={article.slug}>
+            <h2 className="text-4xl font-bold py-8">{article.title}</h2>
+          </a>
+          <p className="text-gray-600 leading-[1.8]">{article.excerpt}</p>
+        </div>
+        <Author {...authors[0]} />
+      </div>
+    </div>
+  );
+}
+function SmallArticle() {
+  return <div>positions</div>;
+}
 
 function Articles() {
   return (
-    <section className="flex flex-col items-center py-12 bg-gray-100">
+    <section className="flex flex-col items-center py-12 max-w-7xl mx-auto">
       <h1 className="text-black font-bold text-4xl mb-10">Articles</h1>
       <div className="flex justify-center p-6 flex-wrap">
-        {articles.map((post) => (
-          <Article key={post.id} {...post} />
-        ))}
+        {articles.map((post, idx) =>
+          idx === 0 ? (
+            <BigArticle key={post.id} {...post} />
+          ) : (
+            <Article key={post.id} {...post} />
+          )
+        )}
       </div>
     </section>
   );
