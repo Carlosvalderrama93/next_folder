b4ca6c4439796ad2247e186fc7116d36ede5487b
Carlos Valderrama <_CarlosValderrama@outlook.com>
2025-09-07 12:30:50 -0500

    All components developed rendered.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,18 @@ import Hero from "@/components/hero";
 import Navigation from "@/components/navigation";
 import Articles from "@/components/articles";
 import Job from "@/components/job";
+import Footer from "@/components/footer";
+import { homePageData } from "@/Data/homepage";
 
+const data = homePageData.footer;
 function page() {
   return (
     <>
       <Navigation />
       <Hero />
-      <Articles />
       <Job />
+      <Articles />
+      <Footer {...data} />
     </>
   );
 }
